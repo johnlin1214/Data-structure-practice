b4d73f5e1dd19f65ffcf00c4f04293d6c2af5319
John Lin <johnlin1214@gmail.com>
2014-12-30 14:22:11 -0800

    added insert and set functions

--- ruby_linked_list_practice.rb
+++ ruby_linked_list_practice.rb
@@ -40,7 +40,7 @@ class SinglyLinkedList
   end
 
   def delete(data_point)
-    @head = @head.next_node if @head.data == data_point
+    @head = @head.next_node if @head.data == data_point # case where data_point is the first note
     current_node = @head.next_node
     previous_node = @head
     while current_node.next_node != nil do
@@ -52,6 +52,61 @@ class SinglyLinkedList
         current_node = current_node.next_node
     end
     puts "#{data_point} not found in list."
+  end
+
+  def get(index) # find the node at index
+    current_index = 1
+    current = @head
+    return current if index == 1
+    while current.next_node != nil do
+      current = current.next_node
+      current_index += 1
+      return current if current_index == index
+    end
+    puts "Not a valid index"
+  end
+
+  def sizeOfLinkedList
+    current = @head
+    size_count = 1
+    until current.next_node == nil
+      size_count += 1
+      current = current.next_node
+    end
+    size_count
+  end
+
+  def outOfBounds?(index)
+    raise IndexError if index < 1 || index > sizeOfLinkedList
+  end
+
+
+  def set(index, data_point) # sets a node's value at a specific index
+    unless outOfBounds?(index)
+      current_index = 1
+      current_node = @head
+      until current_index == index
+        current_node = current_node.next_node
+        current_index += 1
+      end
+      current_node.data = data_point
+    end
+  end
+
+  def insert(index, data_point) # inserts a node at a specific index
+     unless outOfBounds?(index)
+      puts "hello"
+      current_index = 1
+      current_node = @head
+      until current_index == index do
+        previous_node = current_node
+        current_node = current_node.next_node
+        current_index += 1
+      end
+       newNode = Node.new(data_point)
+       previous_node.next_node = newNode
+       newNode.next_node = current_node
+     end
   end
 
   def display_linked_list
@@ -71,12 +126,14 @@ list.addToEndOfList(20)
 list.addToEndOfList(30)
 list.addToFrontOfList(1)
 list.display_linked_list # 1->10->20->30
+puts list.sizeOfLinkedList # 4
 puts list.find_data_point(10) # 10 found at the 2 node
+puts list.find_data_point(100) # 100 not found in list.
 puts list.delete(20) # 20 deleted
+puts list.delete(100) # 100 not found in list.
 list.display_linked_list # 1->10->30
-
-
-
-
-
-
+puts list.get(1) # <Node:0x007f90d40888a8>
+puts list.get(100) # Not a valid index
+list.set(2,15) # 1->15->30
+list.insert(2,20) # 1->20->15->30
+list.display_linked_list
