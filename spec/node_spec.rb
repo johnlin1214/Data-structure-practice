8a66fcfdd3992bcc207e94fd6eed5d47ae7a9207
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2015-01-17 15:13:32 -0800

    complete node and node rspec test

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,16 +1,28 @@
 require_relative "../node"
 
 describe Node do
-  value = "apple"
-  let(:new_node) {Node.new(value)}
+  let(:node) {Node.new(1)}
+  let(:next_node) {Node.new(2)}
+
   describe "#new" do
     it "creates a new instance of a node" do
-      expect(new_node).to be_a Node
+      expect(node).to be_a Node
     end
-
   end
 
+  describe "#insert_after" do
+    it "expects the current node pointer to point to the new node" do
+      node.insert_after(next_node)
+      expect(node.pointer).to eq next_node
+    end
+  end
 
-
+  describe "#remove_after" do
+    it "insert remove the next node" do
+      node.insert_after(next_node)
+      node.remove_after
+      expect(node.pointer).to eq nil
+    end
+  end
 
 end
